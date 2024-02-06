240fa279b084c931e0095d5182f1b72d29a28054
frontenchi <frontendresearch@gmail.com>
2024-02-07 01:44:16 +0300

    getSnapshotBeforeUpdate kullanımı

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg'
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <img src="logo192.png" alt="logo1"></img>
-      <img src={logo} alt="logo1"></img>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {favoritecolor: "red"};
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({favoritecolor: "yellow"})
+    }, 1000)
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    document.getElementById("div1").innerHTML =
+    "Before the update, the favorite was " + prevState.favoritecolor;
+  }
+  componentDidUpdate() {
+    document.getElementById("div2").innerHTML =
+    "The updated favorite is " + this.state.favoritecolor;
+  }
+  render() {
+    return (
+      <div>
+      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+      <div id="div1"></div>
+      <div id="div2"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
